34cd8f841a9ea20d25c2f1165b7264db09da8563
Tk4ee <91469359+Tk4ee@users.noreply.github.com>
2022-02-13 03:46:47 +0200

    new5

---
+++ lab_5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dictt={"(":")","{":")","[":"]"}
+        for i in s:
+            if i in dictt:
+                stack.append(i)
+            elif stack == [] or dictt[stack.pop()]!=i:
+                return False
+        return stack==[]        lab_5.py
