e06ff03f3f779674ee3ffe309b46cdfbf60b0f6c
Tk4ee <91469359+Tk4ee@users.noreply.github.com>
2022-02-13 04:15:02 +0200

    ListNode

--- Vasyl_6.py
+++ Vasyl_6.py
@@ -3,7 +3,7 @@ p = dummy
 p1 = list1
 p2 = list2
 
-while p1 is not None and p2 is not None:
+while p1 is not None and p2 is not None: #2
     if p1.val > p2.val:
         p.next = p2
         p2 = p2.next
