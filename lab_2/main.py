f69c93bd641d90e67e5820afb58b48ddb15621ac
Yura Kuzhiy <yurakuzhiy@gmail.com>
2022-03-29 20:12:47 +0300

    Fix input variable

---
+++ lab_2/main.py
@@ -0,0 +1,9 @@
+import sorting_methods
+
+
+random_array = [1,10,3,8,4,14,2,2]
+sorting_methods.print_sorted(random_array, 'bubble')
+sorting_methods.print_sorted(random_array, 'insert')
+sorting_methods.print_sorted(random_array, 'selection')
+
+sorting_methods.print_sorted(random_array)
