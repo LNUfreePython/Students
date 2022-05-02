2cbc2c557ff4e8703d6ae04305bf954558b5be5d
Yura Kuzhiy <yurakuzhiy@gmail.com>
2022-05-02 14:03:01 +0300

    Refactor

--- lab_3/com_b.py
+++ lab_3/com_b.py
@@ -1,13 +1,9 @@
-#factorial function
+#factorial
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
-
-
-#print(factorial(5))
-
 
 #with repetitions
 def a_n_k(n,k):
@@ -15,40 +11,17 @@ def a_n_k(n,k):
     return 'invalid' 
   return factorial(n)//(factorial(n-k))
 
-
-#print(a_n_k(5,3))
-
-
 #without repetitions
 def _a_n_k(n,k):
-    if k >= n:
-        return 'invalid'
-    return n**k
+  return n**k
 
-
-#print(_a_n_k(5,3))
-
-
-#combination with repetitions
+#combination
 def C(n,k):
-    if k >= n:
-        return 'invalid'
-    return factorial(n)//factorial(k)*factorial(n-k)
-
-
-#print(C(5,3))
+  return factorial(n)//factorial(k)*factorial(n-k)
 
 #combination without repetitions
 def _C(n,k):
-    if k >= n:
-        return 'invalid'
-    return factorial(n + k -1)//factorial(k)*factorial(n-1)
-
-
-#print(_C(5,3))
-
-
-
+  return factorial(n + k -1)//factorial(k)*factorial(n-1)
        
 
 
--- lab_3/main.py
+++ lab_3/main.py
@@ -1,42 +1,19 @@
 from com_b import a_n_k,factorial,_a_n_k,_C,C
-#n = int(input ('Введіть n: '))
-#k = int(input ('Введіть k: '))
 
-#Factorial
+def main():
+  n = int(input ('Введіть n: '))
+  k = int(input ('Введіть k: '))
+  printer('Factorial', factorial(n))
 
-#print (factorial(n))
+  if n > k:
+    printer('Permutation', a_n_k(n,k))
+    printer('Permutation repeated', _a_n_k(n,k))
+    printer('Combination', C(n,k))
+    printer('Combination repeated', _C(n,k))
+  elif k > n:
+    print("k should be lover than n")
 
-#with repetitions
+def printer(text, value):
+  print(text + ': '  + str(value) )
 
-#print (a_n_k(n,k))
-
-#without repetitions
-
-#print (_a_n_k(n,k))
-
-#combination with repetitions
-
-#print (C(n,k))
-
-#combinatio without repetitions
-
-#print (_C(n,k))
-n = int(input ('Введіть n: '))
-k = int(input ('Введіть k: '))
-
-def calc_funcs(a = a_n_k(n,k),b = _a_n_k(n,k),c = factorial(n),d = C(n,k),e = _C(n,k)):
-    if n > k:
-        print (a,b,c,d,e)
-    elif k > n:
-        print ("invalid")
-calc_funcs()        
-    
-
-
- 
-  
-
-    
-
-
-     
+main() 
