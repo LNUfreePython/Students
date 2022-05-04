b895882f7411d91395d7d054e5e3afc73c668565
Yura Kuzhiy <yurakuzhiy@gmail.com>
2022-05-04 13:54:23 +0300

    write file

--- lab_3/Gen_perm22222222.py
+++ lab_3/Gen_perm22222222.py
@@ -1,9 +1,11 @@
 from com_b import factorial
    
-def Gen_perm(A): 
-    for i in range(factorial(len(A))):          
-        print((A))   
-        p=len(A)-1
+def Gen_perm(A):
+    f = open("permutations.txt", "w")
+    for i in range(factorial(len(A))):         
+        print((A))
+        f.write(str(i+1) + ': ' + str(A) + "\n")   
+        p = len(A)-1
         while (p > 0) and (A[p-1] > A[p]):        
             p=p-1
         A[p:] = reversed(A[p:]) 
@@ -12,6 +14,7 @@ def Gen_perm(A):
             while A[p-1] > A[q]:   
                 q=q+1
         A[p-1],A[q]=A[q],A[p-1]
+    f.close() 
     
 A = [1,2,3,4,5,6]
 A = list(A) 
