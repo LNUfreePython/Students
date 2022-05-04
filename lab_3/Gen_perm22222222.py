95ef990d8621894b8576bc3685d644a60ff616ff
Tk4ee <91469359+Tk4ee@users.noreply.github.com>
2022-05-04 15:15:24 +0300

    write file

--- lab_3/Gen_perm22222222.py
+++ lab_3/Gen_perm22222222.py
@@ -1,11 +1,13 @@
 from com_b import factorial
    
 def Gen_perm(A):
+
     f = open("permutations.txt", "w")
     for i in range(factorial(len(A))):         
         print((A))
         f.write(str(i+1) + ': ' + str(A) + "\n")   
         p = len(A)-1
+
         while (p > 0) and (A[p-1] > A[p]):        
             p=p-1
         A[p:] = reversed(A[p:]) 
@@ -14,9 +16,12 @@ def Gen_perm(A):
             while A[p-1] > A[q]:   
                 q=q+1
         A[p-1],A[q]=A[q],A[p-1]
+
     f.close() 
+
+
     
 A = [1,2,3,4,5,6]
 A = list(A) 
 A.sort() 
-Gen_perm(A) 
+Gen_perm(A)
